d3793f4ee10c851f21b8e79e5d84c226e9375a4e
Joanne <joanne.ahn@balancehero.com>
2018-04-18 20:32:10 +0900

    [Joanne][2018.04.18][ADD] add main viewpager

---
+++ app/src/main/java/com/married/got/just/we/ingom/penji/pengom/AnniversaryFragment().kt
@@ -0,0 +1,20 @@
+package com.married.got.just.we.ingom.penji.pengom
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+open class AnniversaryFragment: Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
--- app/src/main/java/com/married/got/just/we/ingom/penji/pengom/MainActivity.kt
+++ app/src/main/java/com/married/got/just/we/ingom/penji/pengom/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.married.got.just.we.ingom.penji.pengom
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentAdapter = MainPagerAdapter(supportFragmentManager)
+        view_pager_main.adapter = fragmentAdapter;
+
+        lyt_main_tab.setupWithViewPager(view_pager_main)
+
+    }
+
+    inner class MainPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        internal var MAX_FRAGMENT_COUNT = 2;
+        private val mottoFragemnt = null
+        private val anniversaryFragment = null
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> MottoFragment()
+                1 -> AnniversaryFragment()
+                else -> return throw IllegalArgumentException("there is no main fragment any more.")
+            }
+        }
+
+        override fun getCount(): Int {
+            return MAX_FRAGMENT_COUNT
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return when(position) {
+                0 -> getString(R.string.motto)
+                1 -> getString(R.string.anniversary)
+                else -> {
+                    ""
+                }
+            }
+        }
     }
 }
